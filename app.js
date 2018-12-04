c2ae5ebc3f988691e0de63c7a353aa5934edb0fc
gritdot <obic55014@gmail.com>
2018-12-04 14:22:45 +0100

    [#develop] Add home page route

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import orderRouter from './server/routes/orderRoute';
 import userRouter from './server/routes/userRoute';
+import router from './server/routes/homeRoutes';
 
 
 const app = express();
@@ -12,6 +13,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/api/v1', userRouter);
 app.use('/api/v1', orderRouter);
+app.use('/', router);
 
 const port = process.env.PORT || 5400;
 
