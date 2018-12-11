9d48779f02548e208a7cbf200a69770f74ea91f4
gritdot <obic55014@gmail.com>
2018-12-11 03:14:19 +0100

    [#162567393] Add update status endpoint

--- server/middleware/orderValidations.js
+++ server/middleware/orderValidations.js
@@ -1,5 +1,8 @@
 import Joi from 'joi';
-import {orderSchema} from './inputScema';
+import {orderSchema, statusUpdate} from './inputScema';
+import pool from '../db/connection';
+import { queryOrdersById } from '../db/sql';
+import { stat } from 'fs';
 
 
 class Validator {
@@ -28,7 +31,44 @@ class Validator {
       next();
   }
 
+  static updateOrderValidator(request, response, next) {
+    
+    const {parcelId} = request.params;
+    pool.query(queryOrdersById, [parcelId])
+      .then((data) => {
+        if (data.rowCount === 0) {
+          return response.status(404)
+            .json({
+              success: false,
+              message: 'This parcel order does not exist'
+            });
+        }
+        if (data.rows[0].status === "Delivered"|| data.rows[0].status === 'Cancelled') {
+          return response.status(406)
+            .json({
+              success: false,
+              message: 'This parcel order is delivered or cancelled'
+            });
+        }
+        
+        const result = Joi.validate(request.body, statusUpdate, { abortEarly: false });
+        if (result.error !== null) {
+          return response.status(400)
+          .json({
+            success: false,
+            message: result.error.message
+          });
+         }
+        next();
+      })
+      .catch(error => response.status(500)
+        .json({
+          success: false,
+          message: error.message
+        }));
+  }
+
 }
 
-const { validateOrder, getAllValidator } = Validator;
-export {validateOrder, getAllValidator};
+const { validateOrder, getAllValidator, updateOrderValidator } = Validator;
+export {validateOrder, getAllValidator, updateOrderValidator};
