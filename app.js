ac0fef60264cb89a89b467ee200f6bac57b5326c
gridot <obic55014@gmail.com>
2018-12-17 18:27:05 +0100

    [#162700571] Add swagger.yaml file

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
 
 import express from 'express';
 import bodyParser from 'body-parser';
+import logger from 'morgan';
+import swaggerUi from 'swagger-ui-express';
+import YAML from 'yamljs';
+import path from 'path';
 import orderRouter from './server/routes/orderRoute';
 import userRouter from './server/routes/userRoute';
 import router from './server/routes/homeRoutes';
 
 
-const app = express();
 
+
+const app = express();
+const swaggerDoc = YAML.load(`${process.cwd()}/swagger.yaml`);
+
+app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 app.use('/api/v1', userRouter);
 app.use('/api/v1', orderRouter);
 app.use('/', router);
